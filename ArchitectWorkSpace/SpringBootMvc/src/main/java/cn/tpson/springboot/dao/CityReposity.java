7609279eeac3d381ebc8b7480cd6d7e0a0b1c8d9
qulei <qulei_20180331@163.com>
2019-08-27 20:19:30 +0800

    删除提交错误的项目

--- ArchitectWorkSpace/SpringBootMvc/src/main/java/cn/tpson/springboot/dao/CityReposity.java
+++
@@ -1,7 +0,0 @@
-package cn.tpson.springboot.dao;
-
-import cn.tpson.springboot.pojo.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityReposity extends JpaRepository<City,Integer> {
-}
--- ArchitectWorkSpace/SpringDemo/src/test/java/cn/test/UserController.java
+++
@@ -1,9 +0,0 @@
-package cn.test;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-    private UserServcie userServcie;
-    public void login(){}
-}
