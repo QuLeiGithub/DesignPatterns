cb1825899833674ffc438831a68c5d09c71f0328
qulei <qulei_20180331@163.com>
2019-08-31 09:48:58 +0800

    ADD | add double lock check

---
+++ DesignPatterns/src/main/java/singleton/Mgr06.java
@@ -0,0 +1,24 @@
+package singleton;
+
+public class Mgr06 {
+    private static volatile Mgr06 INSTANCE;
+
+    private Mgr06() {
+    }
+
+    public static Mgr06 getInstance() {
+        if (INSTANCE == null) {
+            synchronized (Mgr06.class) {
+                if (INSTANCE == null) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+        return INSTANCE;
+    }
+}
